91dff37f631994f7940c4e499dcdb7281488b0f8
Jackcomputers <j.mccotter1@andersonuniversity.edu>
2025-07-15 11:25:35 -0400

    Update phone number again

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -440,7 +440,7 @@ export default function Home() {
                           </div>
                           <div className="flex items-center text-gray-600">
                             <Phone className="w-4 h-4 mr-2" />
-                            <span>Call for bookings and information</span>
+                            <span><a href="tel:+1 (910) 477 1926" className="text-blue-500 hover:underline"> +1 (910) 477 1926</a></span>
                           </div>
                         </div>
                       </div>
@@ -453,7 +453,7 @@ export default function Home() {
                           </div>
                           <div className="flex items-center text-gray-600">
                             <Phone className="w-4 h-4 mr-2" />
-                            <span>Call for bookings and information<a href="tel:+1(910) 477 1926" className="text-blue-500 hover:underline"> +1 (910) 477-1926</a></span>
+                            <span><a href="tel:+1 (912) 434 2937" className="text-blue-500 hover:underline"> +1 (912) 434 2937</a></span>
                           </div>
                         </div>
                       </div>
