6b1970e114dc18632a29758ca733495e7c1fc457
Jackcomputers <j.mccotter1@andersonuniversity.edu>
2025-07-15 11:32:41 -0400

    Update home.tsx

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -436,6 +436,7 @@ export default function Home() {
                         <div className="space-y-2">
                           <div className="flex items-center text-gray-600">
                             <MapPin className="w-4 h-4 mr-2" />
+                            <span>Cape Fear Regional Jetport (KSUT)</span>
                             <span>Southport, North Carolina</span>
                           </div>
                           <div className="flex items-center text-gray-600">
