e89f175ae796c7452c809512a95f9efbdf3b3978
Jackcomputers <j.mccotter1@andersonuniversity.edu>
2025-07-18 13:20:04 -0400

    Update location-card.tsx

--- client/src/components/location-card.tsx
+++ client/src/components/location-card.tsx
@@ -97,7 +97,7 @@ export const LocationCard = ({
               )}
               {hasHelicopter && (
                 <p className="text-sm text-gray-700">
-                  Helicopter tours start at <span className="font-semibold text-ocean-blue">${minHelicopterPrice}</span>
+                  Helicopter tours start at <span className="font-semibold text-ocean-blue">$89</span>
                 </p>
               )}
             </div>
